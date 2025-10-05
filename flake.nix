a79e0cd263b5f91359034a2176f13f632a132e6d
Axel <nigthmaresans2@gmail.com>
2025-10-05 13:19:11 -0600

    refactor: now using flake based envrc

--- flake.nix
+++ flake.nix
@@ -1,34 +1,44 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    nativeBuildInputs = with pkgs; [
-      meson
-      ninja
-      pkg-config
-      vala
-      wrapGAppsHook4
-      blueprint-compiler
-      gobject-introspection
-    ];
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      nativeBuildInputs = with pkgs; [
+        meson
+        ninja
+        pkg-config
+        vala
+        wrapGAppsHook4
+        blueprint-compiler
+        gobject-introspection
+      ];
 
-    buildInputs = with pkgs; [
-      gtk4
-      libadwaita
-      glib
-      gcr_4
-      gtk4-layer-shell
-    ];
-  in {
-    devShells.${system}.default = (pkgs.callPackage ./shell.nix);
+      buildInputs = with pkgs; [
+        gtk4
+        libadwaita
+        glib
+        gcr_4
+        gtk4-layer-shell
+      ];
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        inherit nativeBuildInputs buildInputs;
 
-    packages.${system}.default = pkgs.stdenv.mkDerivation {
-      name = "gprompt";
-      version = "0.1";
-      src = ./.;
-      inherit nativeBuildInputs buildInputs;
+        packages = with pkgs; [
+          vala-language-server
+          uncrustify
+        ];
+      };
+
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        name = "gprompt";
+        version = "0.1";
+        src = ./.;
+        inherit nativeBuildInputs buildInputs;
+      };
     };
-  };
 }
